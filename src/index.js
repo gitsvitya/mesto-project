faf4eb80de96fe99751416800ba5e66d7c639cb1
Ivan Sukhovarov <isukhovarov8@gmail.com>
2022-10-25 21:52:06 +0300

    added popupWithForm.js OOP

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ import FormValidator from '/src/components/validate'
 import Card from '/src/components/card'
 import Section from '/src/components/section.js';
 import PopupWithImage from './components/popupWithImage';
+import PopupWithForm from './components/popupWithForm';
 
 const config = {
   inputSelector: '.popup__input',
@@ -76,74 +77,168 @@ const cardsList = new Section({
   },
 }, '.elements__list');
 
-//======================================================================================================
+//===================================================================================================================
+//     Экземпляр класса, отвечающий за попап с формой редактирования аватара
+//-------------------------------------------------------------------------------------------------------------------
 
-// Добавляем событие "openPopup" на попап с редактированием профиля
+const AvatarPopupWithForm = new PopupWithForm({ 
+  popupSelector: '.popup_avatar-edit', 
+  formSubmit: () => {
+    avatarButton.textContent = 'Сохранение...';
+      api.sendAvatar(avatarInput.value)
+        .then((res) => {
+        avatarImage.style.backgroundImage = `url(${avatarInput.value})`;
+        AvatarPopupWithForm.closePopup();
+      })
+      .catch(err => {
+        console.error(err);
+      })
+      .finally(() => {
+        profileButton.textContent = 'Сохранить';
+      })
+  } 
+});
+
+//===================================================================================================================
+//     Экземпляр класса, отвечающий за попап с формой редактирования профиля
+//-------------------------------------------------------------------------------------------------------------------
+
+const ProfilePopupWithForm = new PopupWithForm({
+  popupSelector: '.popup_profile-edit',
+  formSubmit: () => {
+    profileButton.textContent = 'Сохранение...';
+    api.sendProfileData(nameInput.value, descriptionInput.value)
+      .then((res) => {
+        profileName.textContent = nameInput.value;
+        profileDescription.textContent = descriptionInput.value;
+        ProfilePopupWithForm.closePopup();
+      })
+      .catch(err => {
+       console.error(err);
+      })
+      .finally(() => {
+        profileButton.textContent = 'Сохранить';
+      })
+  }
+});  
+
+//===================================================================================================================
+//     Экземпляр класса, отвечающий за попап с формой добавления новой карточки
+//-------------------------------------------------------------------------------------------------------------------
+
+const CardPopupWithForm = new PopupWithForm({
+  popupSelector: '.popup_add-picture',
+  formSubmit: () => {
+    cardButton.textContent = 'Создание...';
+    api.sendCard(titleInput.value, linkInput.value)
+      .then((res) => {
+       cardsList.addItem(createCard(res));
+       CardPopupWithForm.closePopup();
+      })
+      .catch(err => {
+       console.error(err);
+     })
+      .finally(() => {
+        cardButton.textContent = 'Создать';
+     })
+  }  
+});
+
+//===================================================================================================================
+//     Добавление событий на кнопки 
+//-------------------------------------------------------------------------------------------------------------------
+
+// На попап с добавлением новой карточки
+profileAddButton.addEventListener('click', function () {
+  cardFormValidator.toggleButtonState();
+  CardPopupWithForm.openPopup();
+});
+CardPopupWithForm.setEventListeners();
+
+// На попап с редактированием профиля
 profileEditButton.addEventListener('click', function () {
-  openPopup(popupEditConteiner);
+  ProfilePopupWithForm.openPopup();
+  profileDescriptionFormValidator.toggleButtonState();
   nameInput.value = profileName.textContent;
   descriptionInput.value = profileDescription.textContent;
 });
+ProfilePopupWithForm.setEventListeners();
+
+// На попап с обновления аватара
+avatarEditButton.addEventListener('click', function () {
+  profileAvatarFormValidator.toggleButtonState();
+  AvatarPopupWithForm.openPopup();
+});
+AvatarPopupWithForm.setEventListeners();
+
+//======================================================================================================
+
+// Добавляем событие "openPopup" на попап с редактированием профиля
+// profileEditButton.addEventListener('click', function () {
+//   openPopup(popupEditConteiner);
+//   nameInput.value = profileName.textContent;
+//   descriptionInput.value = profileDescription.textContent;
+// });
 
 // Обработчик сабмита формы
-function handleSubmitCardForm(evt) {
-  cardButton.textContent = 'Сохранение...';
-  evt.preventDefault();
-  api.sendCard(titleInput.value, linkInput.value)
-    .then((res) => {
-      cardsList.addItem(createCard(res));
-      closePopup(popupAddConteiner);
-    })
-    .catch(err => {
-      console.error(err);
-    })
-    .finally(() => {
-      cardButton.textContent = 'Создать';
-    })
-  formAddElement.reset();
-  evt.submitter.classList.add('popup_button-submit-disabled');
-  evt.submitter.setAttribute('disabled', true);
-}
+// function handleSubmitCardForm(evt) {
+//   cardButton.textContent = 'Сохранение...';
+//   evt.preventDefault();
+//   api.sendCard(titleInput.value, linkInput.value)
+//     .then((res) => {
+//       cardsList.addItem(createCard(res));
+//       closePopup(popupAddConteiner);
+//     })
+//     .catch(err => {
+//       console.error(err);
+//     })
+//     .finally(() => {
+//       cardButton.textContent = 'Создать';
+//     })
+//   formAddElement.reset();
+//   evt.submitter.classList.add('popup_button-submit-disabled');
+//   evt.submitter.setAttribute('disabled', true);
+// }
 
 // Функция обновления аватара и отправки на сервер
-function handleProfileAvatarSubmit(event) {
-  avatarButton.textContent = 'Сохранение...';
-  event.preventDefault();
-  api.sendAvatar(avatarInput.value)
-    .then((res) => {
-      avatarImage.style.backgroundImage = `url(${avatarInput.value})`;
-      closePopup(popupAvatarConteiner);
-    })
-    .catch(err => {
-      console.error(err);
-    })
-    .finally(() => {
-      profileButton.textContent = 'Сохранить';
-    })
-}
+// function handleProfileAvatarSubmit(event) {
+//   avatarButton.textContent = 'Сохранение...';
+//   event.preventDefault();
+//   api.sendAvatar(avatarInput.value)
+//     .then((res) => {
+//       avatarImage.style.backgroundImage = `url(${avatarInput.value})`;
+//       closePopup(popupAvatarConteiner);
+//     })
+//     .catch(err => {
+//       console.error(err);
+//     })
+//     .finally(() => {
+//       profileButton.textContent = 'Сохранить';
+//     })
+// }
 
-avatarEditButton.addEventListener('click', function () {
-  openPopup(popupAvatarConteiner);
-});
+// avatarEditButton.addEventListener('click', function () {
+//   openPopup(popupAvatarConteiner);
+// });
 
-closeButtons.forEach((button) => {
-  const popup = button.closest('.popup');
-  button.addEventListener('click', () => closePopup(popup));
-});
+// closeButtons.forEach((button) => {
+//   const popup = button.closest('.popup');
+//   button.addEventListener('click', () => closePopup(popup));
+// });
 
 // Добавляем событие "openPopup" на попап с добавлением новой карточки
-profileAddButton.addEventListener('click', function () {
-  openPopup(popupAddConteiner);
-});
+// profileAddButton.addEventListener('click', function () {
+//   openPopup(popupAddConteiner);
+// });
 
 // Добавляем событие "submit" на попап с редактированием профиля
-formEditElement.addEventListener('submit', handleProfileFormSubmit);
+//formEditElement.addEventListener('submit', handleProfileFormSubmit);
 
 //Добавляем событие "submit" на попап с добавленим новой карточки
-formAddElement.addEventListener('submit', handleSubmitCardForm);
+//formAddElement.addEventListener('submit', handleSubmitCardForm);
 
 // Добавляем событие "submit" на попап с добавлением аватара
-formEditAvatar.addEventListener('submit', handleProfileAvatarSubmit);
+//formEditAvatar.addEventListener('submit', handleProfileAvatarSubmit);
 
 // Функция проверки валидации
 // enableValidation({
